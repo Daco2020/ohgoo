097d3908a673bf8d0b4e72f18a85ddbb4ea8397b
Ted0527 <frosthowling@gmail.com>
2021-11-26 09:38:34 +0900

    [제갈창민] Created models, urls

--- changmin/models.py
+++ changmin/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name       = models.CharField(max_length=20, null=True)
+    email      = models.EmailField(max_length=200, unique=True)
+    password   = models.CharField(max_length=250)
+    contact    = models.CharField(max_length=20, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
